4bd8eb67d4e199314f1458f1592200fc04013ef7
Josef Assad <josef@josefassad.com>
2016-03-22 12:08:13 +0100

    better mysql access from host

--- docker/scripts/initdb.sh
+++ docker/scripts/initdb.sh
@@ -6,6 +6,6 @@ sleep 10
 
 # First we need the mediawiki tables
 mysql -u root -pwiki -e "CREATE DATABASE wiki"
-mysql -u root -pwiki -e "GRANT ALL PRIVILEGES ON wiki.* To 'wiki'@'localhost' IDENTIFIED BY 'wiki';"
+mysql -u root -pwiki -e "GRANT ALL PRIVILEGES ON wiki.* To 'wiki'@'%' IDENTIFIED BY 'wiki';"
 mysql -u root -pwiki wiki < /var/www/wiki/maintenance/tables.sql
 
