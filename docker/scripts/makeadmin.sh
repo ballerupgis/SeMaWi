094d03d606eb19cf358654174c701e7c433c1ea4
Josef Assad <josef@josefassad.com>
2016-09-22 13:04:32 +0200

    opret bot login til gc2 sync

--- docker/scripts/makeadmin.sh
+++ docker/scripts/makeadmin.sh
@@ -7,3 +7,7 @@ sleep 10
 # We'll need a Sysop/Beaureaucrat
 echo "Creating user SeMaWi..."
 php /var/www/wiki/maintenance/createAndPromote.php --bureaucrat --sysop SeMaWi SeMaWiSeMaWi
+
+# We'll need a bot for the GC2 sync
+echo "Creating user Sitebot..."
+php /var/www/wiki/maintenance/createAndPromote.php --bureaucrat --sysop --bot Sitebot SitebotSitebot
