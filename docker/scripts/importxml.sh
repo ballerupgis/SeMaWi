022e2c8f817096b4b81e62e4cdc8cb11ee985c45
Josef Assad <josef@josefassad.com>
2016-11-28 10:32:14 +0100

    fjern gamle mysqld referencer (closes #158)

--- docker/scripts/importxml.sh
+++ docker/scripts/importxml.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-/usr/bin/mysqld_safe &
-
-sleep 10
-
 # Import SeMaWi elements
 php /var/www/wiki/maintenance/importDump.php < /tmp/struktur.xml
 
--- docker/scripts/makeadmin.sh
+++ docker/scripts/makeadmin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-/usr/bin/mysqld_safe &
-
-sleep 10
-
 # We'll need a Sysop/Beaureaucrat
 echo "Creating user SeMaWi..."
 php /var/www/wiki/maintenance/createAndPromote.php --force --bureaucrat --sysop SeMaWi SeMaWiSeMaWi
